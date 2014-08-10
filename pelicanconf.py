ac5308dd78acfb6c8a0666d0ee82e58a80b69759
fspot <fred@fspot.org>
2014-08-10 14:03:01 +0200

    new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
 
-AUTHOR = u"Fred"
-SITENAME = u"Codes"
-SITEURL = 'http://fspot.org/plk/'
-MARKUP=('rst', 'md', 'markdown', 'html')
+AUTHOR = u'@fspot'
+SITENAME = u'Tech blog'
+SITEURL = ''
+
+PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
-DEFAULT_LANG = 'fr'
+DEFAULT_LANG = u'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  ()
+LINKS = ()
 
 # Social widget
 SOCIAL = ()
-DISQUS_SITENAME = 'fspotplk'
 
 DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Theme :
+import os
+THEME = os.path.join(os.path.dirname(__file__), 'pure-theme')
